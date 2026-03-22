73ccd424b9d59e419d38c31182e40cf00323bc0c
Voican Matei <mateivoican2006@gmail.com>
2026-03-22 19:18:50 +0200

    Added BankAccount class. A Client can have up to 5 bank accounts. Stores information about the account including the balance.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,149 @@
 #include <cstring>
 #include <windows.h>
 #include <conio.h>
+#include <unordered_map>
+#include <cmath>
+
+class Investment{
+
+   public:
+      Investment();
+      Investment(double suma, double rata, int perioada);
+
+      //getter
+      double getSumaInitiala() const;
+      double getRataDobanzii() const;
+      int getLuni() const;
+
+      //setteri
+      void setSumaInitiala(double suma);
+      void setRataDobanzii(double rata);
+      void setLuni(int nrLuni);
+
+
+      double calculeazaProfit() const;
+      double getSumaFinala() const;
+
+   private:
+      double sumaInitiala;
+      double rataDobanzii;
+      int luni;
+
+};
+
+Investment::Investment() {
+   this->sumaInitiala = 0;
+   this->rataDobanzii = 0;
+   this->luni = 0;
+}
+
+Investment::Investment(double suma, double rata, int perioada){
+   this->sumaInitiala=suma;
+   this->rataDobanzii=rata;
+   this->luni = perioada;
+}
+
+double Investment::calculeazaProfit() const{
+   double rataLunara = rataDobanzii/ 12.0;
+   double sumaFinala = sumaInitiala * std::pow(1.0 + rataLunara, luni);
+
+   return sumaFinala - sumaInitiala;
+}
+
+class BankAccount{
+   public:
+      BankAccount();
+      BankAccount(int suma);
+      BankAccount(const BankAccount& obj);
+      BankAccount& operator=(const BankAccount& obj);
+
+      friend std::ostream& operator<<(std::ostream& out, const BankAccount& obj);
+      friend std::istream& operator>>(std::istream& in, BankAccount& obj);
+
+      void depunereFonduri(int suma);
+      void retragereFonduri(int suma);
+      int getSold() const;
+      int getID() const;
+      void setSold(int val);
+      void setID(int val);
+
+   private:
+      int sold;
+      int accID;
+
+};
+
+BankAccount::BankAccount(){
+   this->sold = 0;
+   this->accID = rand()%1357;
+}
+
+BankAccount::BankAccount(int val){
+   this->sold = val;
+   this->accID = rand()%1357;   
+}
+
+BankAccount::BankAccount(const BankAccount& obj){
+   this->sold = obj.sold;
+   this->accID = obj.accID;
+}
+
+BankAccount& BankAccount::operator=(const BankAccount& obj){
+   if(this != &obj){
+      this->sold = obj.sold;
+      this->accID = obj.accID;
+   }
+   return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const BankAccount& obj){
+   out << "[Cont ID=" << obj.accID << "] Sold curent: " << obj.sold << " RON";
+   return out;
+}
+
+std::istream& operator>>(std::istream& in, BankAccount& obj){
+   std::cout << "Introduceti depozitul initial: ";
+   in >> obj.sold;
+   return in;
+}
+
+void BankAccount::depunereFonduri(int suma){
+   this->sold += suma;
+}
+
+void BankAccount::retragereFonduri(int suma){
+   if(suma <= this->sold){
+      this->sold -= suma;
+   }
+   else{
+      std::cout << "Fonduri insuficiente!\n";
+   }
+}
+
+int BankAccount::getSold() const{
+   return this->sold;
+}
+
+void BankAccount::setSold(int val){
+   this->sold = val;
+}
+
+int BankAccount::getID() const{
+   return this->accID;
+}
+
+void BankAccount::setID(int ID){
+   this->accID = ID;
+}
 
 
 class Client{
 public:
 
    static int numarClienti;
+
+   BankAccount* conturi[5];
+   int nrConturi;
 
    Client();
    Client(const char* nume, const char* prenume, long long CNP);
@@ -22,6 +159,7 @@ public:
    const char* getNume() const;
    long getID() const;
    long long getCNP() const;
+   void afisareCompletaConturi();
 
    //Clientul isi poate schimba numele..
    void setName(const char* nume);
@@ -37,6 +175,7 @@ private:
    long long CNP;
    long ID_client;
    bool informationComplete;
+   
 };
 
 Client::Client(){
@@ -47,6 +186,9 @@ Client::Client(){
          this->CNP = 0;
          this->ID_client = 0;
          this->informationComplete = false;
+         nrConturi = 0;
+         for(int i = 0; i < 5; i++) this->conturi[i] = nullptr;
+
 }
 
 Client::Client(const char* nume, const char* prenume, long long CNP){
@@ -57,6 +199,8 @@ Client::Client(const char* nume, const char* prenume, long long CNP){
       this->CNP = CNP;
       this->ID_client = CNP % 10000;
       this->informationComplete = false;
+      conturi[0] = new BankAccount();
+      nrConturi = 1;
 }
 
 
@@ -70,6 +214,11 @@ Client::Client(const char* nume, const char* prenume, char initialaTata, int dat
       this->CNP = CNP;
       this->ID_client = CNP % 10000;
       this->informationComplete = true;
+
+      for(int i = 0; i < 5; i++)
+         this->conturi[i] = nullptr;
+      this->conturi[0] = new BankAccount();
+      this->nrConturi = 1;
 };
 
 Client::Client(const Client& obj){
@@ -82,6 +231,12 @@ Client::Client(const Client& obj){
       this->CNP = obj.CNP;
       this->ID_client = obj.ID_client;
       this->informationComplete = obj.informationComplete;
+
+      for(int i = 0; i < 5; i++) this->conturi[i] = nullptr;
+      
+      for (int i = 0; i < obj.nrConturi; i++) {
+        this->conturi[i] = new BankAccount(*obj.conturi[i]);
+    }
 };
 
 Client& Client::operator=(const Client& obj) {
@@ -97,6 +252,18 @@ Client& Client::operator=(const Client& obj) {
          this->CNP = obj.CNP;
          this->ID_client = obj.ID_client;
          this->informationComplete = obj.informationComplete;
+         this->nrConturi = obj.nrConturi;
+
+         for (int i = 0; i < this->nrConturi; i++) {
+             if(this->conturi[i] != nullptr){
+               delete this->conturi[i];
+               this->conturi[i] = nullptr; 
+             }
+        }
+
+         for (int i = 0; i < obj.nrConturi; i++) {
+            this->conturi[i] = new BankAccount(*obj.conturi[i]);
+        }
       }
       return *this;
 };
@@ -104,6 +271,9 @@ Client& Client::operator=(const Client& obj) {
 Client::~Client() {
       delete[] nume;
       delete[] prenume;
+      for (int i = nrConturi-1; i >= 0; i--){
+         delete conturi[i];
+      }
 };
 
 char Client::getInitialaTata() const {
@@ -128,6 +298,12 @@ void Client::setName(const char* nume) {
         this->nume = new char[strlen(nume) + 1];
         strcpy(this->nume, nume);
     }
+}
+
+void Client::afisareCompletaConturi(){
+   for (int i = 0; i < nrConturi; i++){
+      std:: cout << *conturi[i] << std::endl;
+   }
 }
 
 std::ostream& operator<<(std::ostream& out, const Client& obj) {
@@ -158,7 +334,8 @@ private:
 
    Client listaClienti[30];
 
-   void actiuneConectat(Client* cln);
+   int actiuneConectat(Client* cln);
+   void afisareMeniuConectat(const char* nume, const int id);
    void afisareMeniuPrincipal();
    Client* verificareClient();
    Client* creareCont();
@@ -176,8 +353,11 @@ void Menu::run(){
                Client* cln = verificareClient();
                if(cln != nullptr){
                   system("cls");
-                  actiuneConectat(cln);
-                  return;
+                  int next = actiuneConectat(cln);
+                  if (next == 1)
+                     return;
+                  afisareMeniuPrincipal();
+                  break;
                }
                else{
                   std::cout << "Acest cont nu exista.\n";
@@ -192,7 +372,11 @@ void Menu::run(){
                Client *cln = creareCont();
                Sleep(2500);
                system("cls");
-               actiuneConectat(cln);
+               int next = actiuneConectat(cln);
+               if (next == 1)
+                  return;
+               system("cls");
+               afisareMeniuPrincipal();
                break;
             }
             case '3': {
@@ -201,7 +385,7 @@ void Menu::run(){
             }
             case '4': {
                std::cout << "La revedere!\n";
-               // localstorage
+
                Sleep(4000);
                return;
             }
@@ -210,9 +394,112 @@ void Menu::run(){
    }
 }
 
+void Menu::afisareMeniuConectat(const char* nume, const int id){
+   std::cout << "Bine ati venit, Dl./Dna. " << nume << "(Client ID: " << id << ")!\n";
+   std::cout << "1. Vizualizare completa conturi:\n";
+   std::cout << "2. Depunere fonduri\n";
+   std::cout << "3. Retragere fonduri\n";
+   std::cout << "4. Simulare investitii\n";
+   std::cout << "5. Deschideti un cont bancar nou\n";
+   std::cout << "6. Deconectare.\n";
+   std::cout << "7. Inchidere aplicatie.\n";
+}
 
-void Menu::actiuneConectat(Client* cln) {
-   std::cout << "Bine ati venit, Dl./Dna. " << cln->getNume() << "(Client ID: " << cln->getID() << ")!\n";
+int Menu::actiuneConectat(Client* cln) {
+   while(true){
+
+      std::cout<< "\033[?25l";
+      std::cout<< "\033[H";
+      afisareMeniuConectat(cln->getNume(), cln->getID());
+      Sleep(500);
+      if(_kbhit()){
+         char optiune = _getch();
+         switch(optiune){
+            case '1': {
+               system("cls");
+               std::cout << "Conturile dvs.: (Vizibile pentru 10 secunde)\n";
+               cln->afisareCompletaConturi();
+               Sleep(13000);
+               system("cls");
+               break;
+            }
+            case '2': {
+               system("cls");
+               std::cout << "Depunere fonduri:\n";
+               std::cout << "Selectati contul in care doriti sa depuneti fonduri:\n";
+               for (int i = 0; i < cln->nrConturi; i++){
+                  std::cout << "Cont: " << i+1<< " . ID: " << cln->conturi[i]->getID() << std::endl;
+               }
+               int optiuneCont, suma;
+               std::cin >> optiuneCont;
+               BankAccount* contSelectat = cln->conturi[optiuneCont-1];
+               std::cout << "Introduceti suma pe care doriti sa o depuneti (in lei):\n";
+               std::cin >> suma;
+               contSelectat->depunereFonduri(suma);
+               std::cout<<"Gata";
+               Sleep(3000);
+               system("cls");
+               break;
+            }
+            case '3': {
+               system("cls");
+               std::cout << "Retragere fonduri:\n";
+               std::cout << "Selectati contul din care doriti sa retrageti fonduri:\n";
+               for (int i = 0; i < cln->nrConturi; i++){
+                  std::cout << "Cont: " << i+1<< " . ID: " << cln->conturi[i]->getID() << std::endl;
+               }
+               int optiuneCont, suma;
+               std::cin >> optiuneCont;
+               BankAccount* contSelectat = cln->conturi[optiuneCont-1];
+               std::cout << "Introduceti suma pe care doriti sa o retrageti (in lei):\n";
+               std::cin >> suma;
+               contSelectat->retragereFonduri(suma);
+               std::cout<<"Gata";
+               Sleep(3000);
+               system("cls");
+               break;
+            }
+            case '4':{
+               //simulare investitii
+               break;
+            }
+            case '5':{
+               //newbankacc
+               system("cls");
+               if(cln->nrConturi < 5){
+                  std::cout << "Daca doriti un depozit initial, scrieti suma pe care o doriti depozitata: ";
+                  int suma_initiala = 0;
+                  std::cin >> suma_initiala;
+                  if(suma_initiala != 0){
+                     cln->conturi[cln->nrConturi] = new BankAccount(suma_initiala);
+                     
+                  }
+                  else{
+                     cln->conturi[cln->nrConturi] = new BankAccount();
+                  }
+                  cln->nrConturi++;
+                  std::cout << "\nFelicitari. Cont creat. ID cont nou: " << cln->conturi[cln->nrConturi-1]->getID();
+               }
+               else{
+                  std::cout<<"Ati atins numarul maxim de conturi bancare deschise.";
+               }
+               Sleep(4500);
+               system("cls");
+               break;
+            }
+            case '6':{
+               system("cls");
+               return 0;
+            }
+            case '7':{
+               return 1;
+            }
+            default:{
+               break;
+            }
+         }
+      }
+   }
 }
 
 void Menu::afisareMeniuPrincipal() {
@@ -225,6 +512,11 @@ void Menu::afisareMeniuPrincipal() {
 
 Client* Menu::verificareClient(){
       system("cls");
+
+      if (std::cin.peek() == '\n' || std::cin.peek() == '\r') {
+          std::cin.ignore();
+      }
+
       char buffer[20] = "";
       while(strlen(buffer) != 13){
          std::cout << "Introduceti CNP-ul (13 cifre): \n";
@@ -257,11 +549,13 @@ Client* Menu::creareCont(){
          std::cout << "Introduceti un CNP valid (13 cifre): \n";
          std::cin >> CNP;
       }
+      std::cin.ignore();
 
+      Sleep(1000);
       Client temp(nume, prenume, initialaTata, dataNasterii, CNP);
       listaClienti[Client::numarClienti] = temp;
       Client::numarClienti++;
-
+      Sleep(1000);
       std::cout << "Cont creat! ID-ul dvs. este: " << temp.getID() << "\n";
 
       return &listaClienti[Client::numarClienti - 1];
